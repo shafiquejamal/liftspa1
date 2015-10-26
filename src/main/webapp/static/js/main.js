f506f4a834eacce1f0cbb471b5635df73806af59
Shafique Jamal <shafique.jamal@gmail.com>
2015-10-26 01:27:48 -0400

    Simplify the Angular JK. It is not great, but it is better.

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -2,36 +2,7 @@ angular.module('spaModule', [])
     .controller('mainCtrl', mainCtrl);
 
 function mainCtrl($scope) {
-    setView('page1View');
-    $scope.page1 = page1setView;
-    $scope.page2 = page2setView;
-    $scope.page3 = page3setView;
-    $scope.login = loginSetView;
-    $scope.register = registerSetView;
-
-    function setView(view) {
-        $scope.view = view;
-    }
-
-    function page1setView() {
-        setView('page1View');
-    }
-
-    function page2setView() {
-        setView('page2View');
-    }
-
-    function page3setView() {
-        setView('page3View');
-    }
-
-    function loginSetView() {
-        setView('loginView');
-    }
-
-    function registerSetView() {
-        setView('registerView');
-    }
+    $scope.showMe = 'page2';
 
 }
 
